59db84241d73c74b5b0aabdd465ce343fa7e6cb7
Chloe <chenderson126+bitbucket@gmail.com>
2017-03-09 14:55:52 -0600

    Updated code to version released right after class. Runs perfectly.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 "use strict";
 
-var sqlite3 = require('sqlite3').verbose();
+var PORT = 3000;
+
+var fs = require('fs');
 var http = require('http');
+var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
   if(err) console.error(err);
 });
@@ -9,30 +12,33 @@ var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
 var router = new (require('./lib/route').Router);
 
 router.get('/', function(req, res) {
-    fs.readFile('/public/index.html', function(err, body) {
-        res.end(body);
-    });
+  fs.readFile('public/index.html', function(err, body){
+    res.end(body);
+  });
 });
 
 router.get('/app.js', function(req, res) {
-    fs.readFile('/public/app.js', function(err, body) {
-        res.end(body);
-    });
+  fs.readFile('public/app.js', function(err, body){
+    res.end(body);
+  });
 });
 
 router.get('/projects', function(req, res) {
-    db.all('SELECT * FROM projects', [], function(err, projects) {
-        res.setHeader('Content-Type', 'text/json');
-        res.end(JSON.stringify(projects));
-    });
+  db.all('SELECT * FROM projects', [], function(err, projects){
+    res.setHeader('Content-Type', 'text/json');
+    res.end(JSON.stringify(projects));
+  });
 });
 
 var migrate = require('./lib/migrate');
-migrate(db, 'migrations', function(err) {
-    var server = new http.Server(function(req, res) {
-        router.route(req, res);
-    });
-    server.listen(3000, function() {
-        console.log("listening on port " + 3000);
-    });
+migrate(db, 'migrations', function(err){
+
+  var server = new http.Server(function(req, res) {
+    router.route(req, res);
+  });
+  server.listen(PORT, function(){
+    console.log("listening on port " + PORT);
+  });
+
+
 });
